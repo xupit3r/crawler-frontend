4ce8d83797317641fdd606a1bd7d6e9651eaf605
Joe D'Alessandro <joe@thejoeshow.net>
2022-12-03 08:06:02 -0500

    added loading indicator + some more css vairables

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,17 +1,26 @@
 <script setup>
 import { useOverviewStore } from '@/stores/overview';
+import { reactive } from 'vue';
 import DashboardCard from '../components/DashboardCard.vue';
+import Loader from '../components/Loader.vue';
 import RainbowTitle from '../components/RainbowTitle.vue';
 
 const overviewStore = useOverviewStore();
 
-overviewStore.getCounts();
+const state = reactive({
+  loading: true
+});
+
+overviewStore.getCounts().then(() => state.loading = false);
 
 </script>
 
 <template>
   <RainbowTitle title="Dashboard" />
-  <div class="content-flex row-left">
+  <div v-if="state.loading" class="content-centered">
+    <Loader />
+  </div>
+  <div v-else class="content-flex row-left">
     <DashboardCard v-for="count in overviewStore.counts"
                    :key="count.name"
                    :title="count.name"
