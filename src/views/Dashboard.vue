11e069eecca61c8fcbf30cec0ffde90bf48b7d59
Joe D'Alessandro <joe@thejoeshow.net>
2022-12-03 08:48:55 -0500

    added polling to the dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { useOverviewStore } from '@/stores/overview';
-import { reactive } from 'vue';
+import { onUnmounted, reactive } from 'vue';
 import DashboardCard from '../components/DashboardCard.vue';
 import Loader from '../components/Loader.vue';
 import RainbowTitle from '../components/RainbowTitle.vue';
@@ -8,18 +8,30 @@ import RainbowTitle from '../components/RainbowTitle.vue';
 const overviewStore = useOverviewStore();
 
 const state = reactive({
-  loading: true
+  loading: true,
+  poller: null
 });
 
-overviewStore.getCounts().then(() => state.loading = false);
+overviewStore.getCounts().then(() => {
+  state.loading = false;
+  const refresher = () => {
+    setTimeout(() => {
+      overviewStore.getCounts().finally(refresher);
+    }, 500);
+  };
+
+  refresher();
+});
+
+onUnmounted(() => {
+  clearTimeout(state.poller);
+});
 
 </script>
 
 <template>
   <RainbowTitle title="Dashboard" />
-  <div v-if="state.loading" class="content-centered">
-    <Loader />
-  </div>
+  <Loader v-if="state.loading" />
   <div v-else class="content-flex row-left">
     <DashboardCard v-for="count in overviewStore.counts"
                    :key="count.name"
