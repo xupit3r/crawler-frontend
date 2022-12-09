9e7d0b8a1aa51fe897f438a27c7cb8506b737270
Joe D'Alessandro <joe@thejoeshow.net>
2022-12-09 05:44:21 -0500

    added some basic filtering to the page listings

--- src/components/PageCard.vue
+++ src/components/PageCard.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { reactive } from '@vue/reactivity';
 import { useRouter } from 'vue-router';
 
 const props = defineProps({
@@ -10,17 +9,6 @@ const props = defineProps({
 });
 
 const router = useRouter();
-
-const state = reactive({
-  status: props.page.status ? props.page.status : '?',
-  counts: {
-    links: props.page.links.length,
-    hosts: Object.values(props.page.links.reduce((h, link) => {
-      h[link.host] = true;
-      return h;
-    }, {})).length
-  }
-})
 
 const loadPage = () => {
   router.push({
@@ -36,8 +24,5 @@ const loadPage = () => {
 <template>
   <div class="card pageCard" @click.stop="loadPage">
     <span class="pageCard-url">{{page.url}}</span>
-    <span class="card-meta pageCard-status">{{state.status}}</span>
-    <span class="card-meta pageCard-linkCount">{{state.counts.links}}</span>
-    <span class="card-meta pageCard-uniqueHosts">{{state.counts.hosts}}</span>
   </div>
 </template>
--- src/views/Pages.vue
+++ src/views/Pages.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { reactive, computed } from 'vue';
+import { debounce } from 'debounce';
 import RainbowNav from '@/components/RainbowNav.vue';
 import PageCard from '@/components/PageCard.vue';
 import { usePagesStore } from '@/stores/pages';
@@ -9,7 +10,8 @@ const pagesStore = usePagesStore();
 
 const state = reactive({
   loading: true,
-  show: 100
+  show: 100,
+  filter: ''
 });
 
 const nav = [{
@@ -21,12 +23,22 @@ const nav = [{
 }];
 
 const shownPages = computed(() => {
-  return pagesStore.pages.slice(0, state.show);
+  return pagesStore.pages.filter(page => {
+    if (state.filter.length > 2) {
+      return page.url.indexOf(state.filter) > -1;
+    }
+
+    return true;
+  }).slice(0, state.show);
 });
 
 function showMore () {
   state.show = state.show + 100;
 }
+
+const doFilter = debounce((ev) => {
+  state.filter = ev.target.value;
+}, 250);
 
 pagesStore.getPages().finally(() => state.loading = false);
 </script>
@@ -34,7 +46,12 @@ pagesStore.getPages().finally(() => state.loading = false);
 <template>
   <RainbowNav :nav="nav" />
   <Loader v-if="state.loading" />
-
+  <div v-if="!state.loading" class="content-actions">
+    <label class="input-field">
+      <div class="label">Filter</div>
+      <input type="text" name="search" class="input-search" @input.stop="doFilter" />
+    </label>
+  </div>
   <div v-if="!state.loading " class="content-flex row row-wrap">
     <PageCard v-for="page in shownPages" :page="page" />
   </div>
