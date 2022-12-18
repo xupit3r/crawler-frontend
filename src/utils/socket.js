a2b5d462fe2946891534bcd6db074bf1506c605d
Joe D'Alessandro <joe@thejoeshow.net>
2022-12-18 07:52:16 -0500

    added socket reconnection

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -10,42 +10,48 @@ export const useSocket = () => {
   const overviewStore = useOverviewStore();
   const staticStore = useStaticStore();
   const SOCKET_URL = `ws://${location.hostname}:${location.port}/ws`;
+
+  const onMessage = ev => {
+    const data = JSON.parse(ev.data);
   
-  if (state.socket === null) {
-    try {
-      state.socket = new WebSocket(SOCKET_URL);
-    } catch (err) {
-      console.error('could not connect to socket', err);
-    }
-  
-    state.socket.addEventListener('message', ev => {
-      const data = JSON.parse(ev.data);
-    
-      if (data.type === 'dashboard') {
-        overviewStore.counts = data.counts.map(count => ({
-          ...count,
-          ...{
-            value: count.value.toLocaleString('en-US'),
-            link: countToLink(count.name)
-          }
-        }));
-  
-        overviewStore.upNext = data.upNext;
-        overviewStore.cooldown = data.cooldown;
-      } else if (data.type === 'page') {
-        staticStore.addPage(data.page);
-      } else if (data.type === 'tf') {
-        if (data.tf){ 
-          staticStore.addTF(data.tf);
-        } else if (data.done) {
-          staticStore.tfLoaded();
+    if (data.type === 'dashboard') {
+      overviewStore.counts = data.counts.map(count => ({
+        ...count,
+        ...{
+          value: count.value.toLocaleString('en-US'),
+          link: countToLink(count.name)
         }
+      }));
+
+      overviewStore.upNext = data.upNext;
+      overviewStore.cooldown = data.cooldown;
+    } else if (data.type === 'page') {
+      staticStore.addPage(data.page);
+    } else if (data.type === 'tf') {
+      if (data.tf){ 
+        staticStore.addTF(data.tf);
+      } else if (data.done) {
+        staticStore.tfLoaded();
       }
-    });
+    }
+  };
+  
+  const connectSocket = () => {
+    if (state.socket === null) {
+      state.socket = new WebSocket(SOCKET_URL);
+      state.socket.addEventListener('error', () => {
+        state.socket = null;
+        setTimeout(connectSocket, 100);
+      });
+
+      state.socket.removeEventListener('message',onMessage);
+      state.socket.addEventListener('message', onMessage);
+    }
   }
 
   const socketSend = (data) => {
-    if (state.socket.readyState !== 1) {
+    if (state.socket === null ||
+        state.socket.readyState !== 1) {
       return setTimeout(() => {
         socketSend(data);
       }, 10);
@@ -53,6 +59,8 @@ export const useSocket = () => {
 
     state.socket.send(JSON.stringify(data));
   }
+
+  connectSocket();
 
   return {
     socketSend,
